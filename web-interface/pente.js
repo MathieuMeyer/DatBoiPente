09acbccb8d35613aa0da52f787308bb53b01b6dd
paulbore <bore-paul@voila.fr>
2017-04-12 16:19:08 +0200

    added clock scoreboard improvement

--- web-interface/pente.js
+++ web-interface/pente.js
@@ -1,84 +1,77 @@
 var model = {
-  playerOneName: "",
-  playerTwoName: "",
-  boardSize: 20,
-  currentPlayerColor: 'white',
-  currentMoveX: null,
-  currentMoveY: null,
-  pairsStolen: [],
-  won: null,
-  whiteStolenPairs: 0,
-  blackStolenPairs: 0,
-  pieces: {},
+	playerOneName: "",
+	playerTwoName: "",
+	boardSize: 19,
+	currentPlayerColor: 'white',
+	currentMoveX: null,
+	currentMoveY: null,
+	pairsStolen: [],
+	won: null,
+	whiteStolenPairs: 0,
+	blackStolenPairs: 0,
+	pieces: {},
 
-  currentOpponentColor: function() {
-    if (this.currentPlayerColor == 'white') {
-      return 'black';
-    } else {
-      return 'white';
-    }
-  },
+	currentOpponentColor: function() {
+		if (this.currentPlayerColor == 'white') {
+			return 'black';
+		} else {
+			return 'white';
+		}
+	},
 
-  currentPlayerName: function() {
-    if (this.currentPlayerColor == "white") {
-      return this.playerOneName;
-    } else {
-      return this.playerTwoName;
-    }
-  },
+	currentPlayerName: function() {
+		if (this.currentPlayerColor == "white") {
+			return this.playerOneName;
+		} else {
+			return this.playerTwoName;
+		}
+	},
 
 
-  resetGame: function() {
-    this.pieces = {};
-    this.won = "";
-    this.currentPlayerColor = "white";
-    this.whiteStolenPairs = 0;
-    this.blackStolenPairs = 0;
-    this.currentPlayerHuman = true;
-    this.won = null;
-    this.pieces = {};
-    this.pairsStolen = [];
-    view.initializeBoard();
-  }
-
+	resetGame: function() {
+	this.pieces = {};
+	this.won = "";
+	this.currentPlayerColor = "white";
+	this.whiteStolenPairs = 0;
+	this.blackStolenPairs = 0;
+	this.currentPlayerHuman = true;
+	this.won = null;
+	this.pieces = {};
+	this.pairsStolen = [];
+	view.initializeBoard();
+	}
 }
 
 var view = {
 
-  initializeBoard: function() {
-    var boardTarget = document.getElementById('gameBoard');
-    boardTarget.innerHTML = "";
-    for (var i = 1; i < model.boardSize; i ++) {
-      var row = document.createElement('tr');
-      for (var j = 1; j < model.boardSize; j ++) {
-        var cell = document.createElement('td');
-        var column = i.toString();
-        var roww = j.toString();
-        if (column.length < 2) {
-          column = "0" + column;
-        }
-        if (roww.length < 2) {
-          roww = "0" + roww;
-        }
-        var cellId = roww + column;
-        cell.setAttribute('id', cellId);
-        row.appendChild(cell);
-      }
-      boardTarget.appendChild(row);
-    }
+	initializeBoard: function() {
+		var boardTarget = document.getElementById('gameBoard');
+		boardTarget.innerHTML = "";
+		for (var i = 0; i < model.boardSize; i ++) {
+			var row = document.createElement('tr');
+			for (var j = 0; j < model.boardSize; j ++) {
+				var cell = document.createElement('td');
+				var column = i.toString();
+				var roww = j.toString();
+				if (column.length < 2) {
+					column = "0" + column;
+				}
+				if (roww.length < 2) {
+					roww = "0" + roww;
+				}
+				var cellId = roww + column;
+				cell.setAttribute('id', cellId);
+				row.appendChild(cell);
+			}
+			boardTarget.appendChild(row);
+		}
+		this.resizeBoard();
+	},
 
-    this.resizeBoard();
-    document.getElementById('stolenWhite').innerHTML = "";
-    document.getElementById('stolenBlack').innerHTML = "";
-    document.getElementById('options').innerHTML = "Options";
-    this.displayMsg('');
-    // this.displayMsg('Current move: ' + model.currentPlayerColor);
-  },
-
-  resizeBoard: function() {
-    var $cells = $('#gameBoard td');
-    $cells.height($cells.width());
-  },
+	resizeBoard: function() {
+		var $cells = $('#gameBoard td');
+		$cells.height($cells.width());
+	},
 
   placeStone: function(position, color) {
     position = document.getElementById(position);
@@ -103,10 +96,6 @@ var view = {
       view.displayMsg('Current move: ' + model.currentPlayerColor);
     }
   },
-
-  // displayAuxMsg: function(message) {
-  //   document.getElementById('auxMessageArea').innerHTML = message;
-  // },
 
   displayStolenPairs: function() {
     console.log('processing stolen pairs images')
@@ -144,10 +133,28 @@ var view = {
       model.resetGame();
     }
 
-
-
 window.onload = function() {
   view.initializeBoard();
   $(window).resize(view.resizeBoard);
-  var el = document.getElementById('gameBoard');
 }
+
+var clock;
+		
+$(document).ready(function() {
+	var clock;
+
+	clock = $('.clock').FlipClock({
+		clockFace: 'MinuteCounter',
+		autoStart: false,
+		language: 'fr',
+		countdown: true,
+		callbacks: {
+			stop: function() {
+				$('.clock-message').html('Mort subite !')
+			}
+		}
+	});		
+	clock.setTime(600);
+	clock.start();
+
+});
