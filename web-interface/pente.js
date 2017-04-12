590c8d1b1e830a86ab70be202d397dd6c7e46f11
Xavier <cobigoxavier@gmail.com>
2017-04-13 00:56:11 +0200

    relation interface-serveur

--- web-interface/pente.js
+++ web-interface/pente.js
@@ -1,148 +1,47 @@
 var model = {
-	playerOneName: "",
-	playerTwoName: "",
-	boardSize: 19,
-	currentPlayerColor: 'white',
-	currentMoveX: null,
-	currentMoveY: null,
-	pairsStolen: [],
-	won: null,
-	whiteStolenPairs: 0,
-	blackStolenPairs: 0,
-	pieces: {},
-
-	currentOpponentColor: function() {
-		if (this.currentPlayerColor == 'white') {
-			return 'black';
-		} else {
-			return 'white';
-		}
-	},
-
-	currentPlayerName: function() {
-		if (this.currentPlayerColor == "white") {
-			return this.playerOneName;
-		} else {
-			return this.playerTwoName;
-		}
-	},
-
-
-	resetGame: function() {
-	this.pieces = {};
-	this.won = "";
-	this.currentPlayerColor = "white";
-	this.whiteStolenPairs = 0;
-	this.blackStolenPairs = 0;
-	this.currentPlayerHuman = true;
-	this.won = null;
-	this.pieces = {};
-	this.pairsStolen = [];
-	view.initializeBoard();
-	}
+		boardSize: 19
 }
 
 var view = {
 
-	initializeBoard: function() {
-		var boardTarget = document.getElementById('gameBoard');
-		boardTarget.innerHTML = "";
-		for (var i = 0; i < model.boardSize; i ++) {
-			var row = document.createElement('tr');
-			for (var j = 0; j < model.boardSize; j ++) {
-				var cell = document.createElement('td');
-				var column = i.toString();
-				var roww = j.toString();
-				if (column.length < 2) {
-					column = "0" + column;
+		initializeBoard: function() {
+			var boardTarget = document.getElementById('gameBoard');
+			boardTarget.innerHTML = "";
+			for (var i = 0; i < model.boardSize; i ++) {
+				var row = document.createElement('tr');
+				for (var j = 0; j < model.boardSize; j ++) {
+					var cell = document.createElement('td');
+					var column = i.toString();
+					var roww = j.toString();
+					if (column.length < 2) {
+						column = "0" + column;
+					}
+					if (roww.length < 2) {
+						roww = "0" + roww;
+					}
+					var cellId = roww + column;
+					cell.setAttribute('id', cellId);
+					row.appendChild(cell);
 				}
-				if (roww.length < 2) {
-					roww = "0" + roww;
-				}
-				var cellId = roww + column;
-				cell.setAttribute('id', cellId);
-				row.appendChild(cell);
+				boardTarget.appendChild(row);
 			}
-			boardTarget.appendChild(row);
-		}
-		this.resizeBoard();
-	},
+			this.resizeBoard();
+		},
 
-	resizeBoard: function() {
-		var $cells = $('#gameBoard td');
-		$cells.height($cells.width());
-	},
+		resizeBoard: function() {
+			var $cells = $('#gameBoard td');
+			$cells.height($cells.width());
+		},
 
-  placeStone: function(position, color) {
-    position = document.getElementById(position);
-    position.className = color;
-  },
-
-  removeStone: function(position) {
-    position = document.getElementById(position); // 'cause now 'position' is returning the coordinates, not the acutal element
-    position.className = '';
-    console.log('removing ' + position);
-  },
-
-  displayMsg: function(message) {
-    document.getElementById('msgArea').innerHTML = message;
-  },
-
-  updateMoveMsg: function() {
-    console.log('updateMoveMsg');
-    if (model.twoPlayer) {
-      view.displayMsg('Current move: ' + model.currentPlayerName() + ' (' +  model.currentPlayerColor + ')');
-    } else {
-      view.displayMsg('Current move: ' + model.currentPlayerColor);
-    }
-  },
-
-  displayStolenPairs: function() {
-    console.log('processing stolen pairs images')
-    var whiteString = "";
-    var blackString = "";
-    for (i = 0; i < model.whiteStolenPairs; i ++) {
-      whiteString += '<img src="images/stolen-white.png" alt="Stolen White Pair">';
-    }
-    for (i = 0; i < model.blackStolenPairs; i ++) {
-      blackString += '<img src="images/stolen-black.png" alt="Stolen White Pair">';
-    }
-    console.log(whiteString);
-    console.log(blackString);
-    document.getElementById('stolenWhite').innerHTML = whiteString;
-    document.getElementById('stolenBlack').innerHTML = blackString;
-  },
-
-  displayWin: function() {
-    var winString = 'Game over: ';
-    if (model.twoPlayer) {
-      winString += model.currentPlayerName() + ' (' +  model.currentPlayerColor + ') wins.';
-    } else {
-      if (model.currentPlayerHuman) {
-        winString += 'You win. (' + model.currentPlayerColor + ')';
-      } else {
-        winString += 'Computer wins. (' + model.currentPlayerColor + ')';
-      }
-  }
-  
-  },
-
-    }
-
-    if (model.won) {
-      model.resetGame();
-    }
-
-window.onload = function() {
-  view.initializeBoard();
-  $(window).resize(view.resizeBoard);
 }
 
-var clock;
-		
+window.onload = function() {
+		view.initializeBoard();
+		$(window).resize(view.resizeBoard);
+}
+			
 $(document).ready(function() {
 	var clock;
-
 	clock = $('.clock').FlipClock({
 		clockFace: 'MinuteCounter',
 		autoStart: false,
@@ -156,5 +55,30 @@ $(document).ready(function() {
 	});		
 	clock.setTime(600);
 	clock.start();
-
 });
+
+setInterval(function() {
+	$.get( "http://localhost:3000/turn/B1hbeVnpg", function( data ) {
+		for(var x = 0; x<data.tableau.length; x++){
+			for(var y = 0; y<data.tableau.length; y++){
+				var coordX = x.toString();
+				var coordY = y.toString();
+				if (coordX.length < 2) {
+					coordX = "0" + coordX;
+				}
+				if (coordY.length < 2) {
+					coordY = "0" + coordY;
+				}
+				switch(data.tableau[x][y]){
+					case 1:
+						$('#'+coordX+coordY).html('<p>O</p>');
+						break;
+					case 2:
+						$('#'+coordX+coordY).html('<p>X</p>');
+						break;
+				}
+			}
+		}
+
+	});
+},500);
